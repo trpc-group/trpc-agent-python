913eff7701c3892dfdf2c852a8f781d3e29dcf59
xyaohubery <jim753124@126.com>
2026-07-12 16:32:28 +0800

    fix: correct default model to hy3

--- examples/code_review_agent/agent/config.py
+++ examples/code_review_agent/agent/config.py
@@ -4,5 +4,5 @@ import os
 def get_model_config():
     api_key = os.environ.get("TRPC_AGENT_API_KEY", "EMPTY")
     url = os.environ.get("TRPC_AGENT_BASE_URL", "http://127.0.0.1:8000/v1")
-    model_name = os.environ.get("TRPC_AGENT_MODEL_NAME", "tencent/Hy3")
+    model_name = os.environ.get("TRPC_AGENT_MODEL_NAME", "hy3")
     return api_key, url, model_name
